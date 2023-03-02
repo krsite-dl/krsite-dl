fa2afc3c44556d40d67a7a82c4914e261269d5ba
Dan <neptunemist@proton.me>
2023-03-02 14:18:38 +0700

    Add files via upload

--- krsite-dl.py
+++ krsite-dl.py
@@ -10,39 +10,45 @@ from selenium.webdriver.chrome.options import Options
 
 parser = argparse.ArgumentParser()
 parser.add_argument("url", nargs='?',type=str, help="valid news/blog url")
-parser.add_argument("-a", type=str, help="text file containing urls")
+parser.add_argument("-a", type=str, help="text file containing site urls")
+parser.add_argument("-ai", type=str, help="text file containing image urls")
+# parser.add_argument("--board-no", type=int, help="The board number from a site (SBS). For example (https://programs.sbs.co.kr/enter/gayo/visualboard/54795?cmd=view&page=1&board_no=438994) will have a board no of 438994")
 parser.add_argument("-d", "--destination", type=str, default=".",help="The destination path for the downloaded file")
 args = parser.parse_args()
      
         
 def check_site(url):
-    '''check url name'''
+    url = url.strip()
+    
     if 'dispatch.co.kr' in url:
         print("Site name 'Dispatch'")
         from_dispatch(url)
-    elif 'news.imbc.com' in url:
+    elif 'enews.imbc.com' in url:
         print("Site name 'iMBC News'")
         from_imbcnews(url)
+    elif 'newsjamm.co.kr' in url:
+        print("Site name 'News Jamm'")
+        from_newsjamm(url)
     elif 'sbs.co.kr' in url:
         print("Site name 'SBS'")
         from_sbs(url)
     elif 'post.naver.com' in url:
-        print("Site name 'Naver 포스트")
+        print("Site name 'Naver 포스트'")
         from_naverpost(url)
     else:
-        print("URL invalid / Site not supported. [%s]" % url)
+        # print("URL invalid / Site not supported. [%s]" % url)
+        print("Generic Sites %s *may not work" % url.split('/')[2])
+        from_generic(url)
         
-        
+
 def from_dispatch(hd):
     r = requests.get(hd)
-    
     soup = BeautifulSoup(r.text, 'html.parser')
-
     img_list = []
-    
-    items = soup.findAll('img', class_='post-image')
-    
-    for i in items:
+    post_date = soup.find('div', class_='post-date').string.replace('.', '').strip()[:8]
+    post_title = soup.find('div', class_='page-post-title').string.strip()
+
+    for i in soup.findAll('img', class_='post-image'):
         if i.get('data-src') != None:
             temp = i.get('data-src')
             img_list.append(temp)
@@ -50,38 +56,57 @@ def from_dispatch(hd):
             temp = i.get('src')
             img_list.append(temp)
     
+    print("Title: %s" % post_title)
+    print("Date: %s" % post_date[2:])
     print("Found %s image(s)" % len(img_list))
     
-    dir_handler(img_list)
+    dir_handler_alt(img_list, post_title, post_date[2:])
     
     
 def from_imbcnews(hd):
     r = requests.get(hd)
     soup = BeautifulSoup(r.text, 'html.parser')
-    img_list = []
     post_title = soup.find('h2').text
     post_date = soup.find('span', class_='date').string.replace('-', '')[:8]
+    img_list = []
+
+    print("Title: %s" % post_title)
+    print("Date: %s" % post_date)
 
     for item in soup.findAll('img'):
         if item.get('src') is not None:
-            if 'talkimg' in item.get('src'):
+            if 'talkimg.imbc.com' in item.get('src'):
                 img_list.append('http:' + item.get('src'))
     
-    img_list1 = list(set(img_list))
+    print("Found %s image(s)" % len(img_list))
+    dir_handler(img_list, post_title, post_date)
     
-    print("Found %s image(s)" % len(img_list1))
-    dir_handler(img_list1, post_title, post_date)
-    
+
+def from_newsjamm(hd):
+    r = requests.get(hd)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    post_title = soup.find('h1').text
+    post_date = soup.find('span', class_='PostContent_statusItem__AgJEE').string.replace('.', '')
+    img_list = []
+
+    for item in soup.findAll('img'):
+        img_list.append(item.get('src'))
+
+    print("Found %s image(s)" % len(img_list))
+
+    dir_handler(img_list, post_title, post_date)
+
 
 def from_sbs(hd):
     board_id = hd.split('=')[-1]
+    print("Board no: %s" % board_id)
 
-    ex = False
+    code = ''
+
     if '69423' in hd:
         code = 'runningman_photo'
     elif '65942' in hd and 'pdnote_hotissue' in hd:
         code = 'pdnote_hotissue'
-        ex = True
     elif '54795' in hd or '65942' in hd:
         code = 'inkigayo_pt01'
     elif '68458' in hd:
@@ -90,17 +115,18 @@ def from_sbs(hd):
         code = 'inkigayo_pt05'
     elif '76371' in hd:
         code = '2022sbsgayo_pt'
-    
+    elif '58358' in hd:
+        code = 'theshow04_pt'
+    # add more to your liking. (elif 'board_id' in hd: code = 'board_code')
 
     ###########TOKEN############
-    token = '1675499213471'
+    token = '1676983404524'
     ############################
 
     api = 'https://api.board.sbs.co.kr/bbs/V2.0/basic/board/detail/'
-    params = '?callback=boardViewCallback_%s&action_type=callback&board_code=%s&jwt-token=&_=%s' % (code, code, token)
+    params = '%s?callback=boardViewCallback_%s&action_type=callback&board_code=%s&jwt-token=&_=%s' % (board_id, code, code, token)
 
-    r = requests.get(api + board_id + params)
-    '''print(api + board_id + params)'''
+    r = requests.get(api + params)
     json_data = r.text
     json_data = json_data.split('boardViewCallback_%s(' % code)[1]
     json_data = json_data.replace(');', '')
@@ -115,12 +141,11 @@ def from_sbs(hd):
     print("Date: %s" % post_date)
 
     for i in data['URL']:
-        if ex == True:
+        if 'http' not in i:
             img_list.append('http:' + str(i))
         else:
-            if 'image.board.sbs.co.kr' not in i:
-                img_list.append(str(i))
-            
+            img_list.append(str(i))
+    
     
     print("Found %s image(s)" % len(img_list))
 
@@ -189,9 +214,19 @@ def from_naverpost(hd):
     elif 'postView.naver' in hd:
         print("Post")
         naverpost_post(hd)
+
+
+def from_generic(hd):
+    r = requests.get(hd)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    img_list = []
+
+    for item in soup.findAll('img'):
+        img_list.append(item.get('src'))
     
-    
-    
+    print("Found %s image(s)" % len(img_list))
+
+    dir_handler(img_list)
 
   
 def dir_handler(img_list, title = None, date = None):
@@ -205,6 +240,25 @@ def dir_handler(img_list, title = None, date = None):
             os.makedirs(dirs)
 
     download_handler(img_list, dirs)
+
+
+def dir_handler_alt(img_list, title = None, date = None):
+    if title != None and date != None:
+        dirs = args.destination + '/' + date
+        subdirs = dirs + '/' + title
+        if not os.path.exists(dirs):
+            os.makedirs(dirs)
+            if not os.path.exists(dirs + '/' + title):               
+                os.makedirs(subdirs)
+        else:
+            if not os.path.exists(dirs + '/' + title):
+                os.makedirs(subdirs)
+    else:
+        dirs = args.destination
+        if not os.path.exists(dirs):
+            os.makedirs(dirs)
+
+    download_handler(img_list, subdirs)
     
 
 def download_handler(img_list, dirs, chunk_size = 128):
@@ -219,41 +273,42 @@ def download_handler(img_list, dirs, chunk_size = 128):
         print("\n[Image] %s" % img_name)
 
         print("[Metadata] extracting metadata...")
-        url_mod_time = response.headers.get('Last-Modified')
 
         if os.path.exists(dirs + '/' + img_name):
             print("[Status] This file already exists. Skipping...")
             continue
-        else:
-            try:
-                current_size = os.path.getsize(dirs + "/" + img_name + '.part')
-            except FileNotFoundError:
-                current_size = 0
-            
-            if current_size < content_length:
-                headers = {'Range': f'bytes={current_size}-{content_length-1}'}
-                response = requests.get(img, headers=headers, stream=True)
+        
+        url_mod_time = response.headers.get('Last-Modified')
 
-                with open(dirs + '/' + img_name + '.part', 'ab') as f:
-                    start = time.time()
-                    for chunk in response.iter_content(chunk_size=chunk_size):
-                        current_size += len(chunk)
-                        f.write(chunk)
-                        f.flush()
-                        progress_handler(current_size, content_length, start)
-                
-                os.rename(dirs + '/' + img_name + '.part', dirs + '/' + img_name)
-                
-                print("\n[Status] Image %s downloaded" % img_name)
+        try:
+            current_size = os.path.getsize(dirs + "/" + img_name + '.part')
+        except FileNotFoundError:
+            current_size = 0
+        
+        if current_size < content_length:
+            headers = {'Range': f'bytes={current_size}-{content_length-1}'}
+            response = requests.get(img, headers=headers, stream=True)
 
-                # Set file and folders modification time
-                if url_mod_time:
-                    print("[Metadata] Embedding metadata to %s" % img_name)
-                    mod_time = time.strptime(url_mod_time, '%a, %d %b %Y %H:%M:%S %Z')
-                    os.utime(dirs + '/' + img_name, (time.time(), time.mktime(mod_time)))
+            with open(dirs + '/' + img_name + '.part', 'ab') as f:
+                start = time.time()
+                for chunk in response.iter_content(chunk_size=chunk_size):
+                    current_size += len(chunk)
+                    f.write(chunk)
+                    f.flush()
+                    progress_handler(current_size, content_length, start)
             
+            os.rename(dirs + '/' + img_name + '.part', dirs + '/' + img_name)
+            
+            print("\n[Status] Image %s downloaded" % img_name)
+
+            # Set file and folders modification time
             if url_mod_time:
-                os.utime(dirs, (time.time(), time.mktime(mod_time)))
+                print("[Metadata] Embedding metadata to %s" % img_name)
+                mod_time = time.strptime(url_mod_time, '%a, %d %b %Y %H:%M:%S %Z')
+                os.utime(dirs + '/' + img_name, (time.time(), time.mktime(mod_time)))
+        
+        if url_mod_time:
+            os.utime(dirs, (time.time(), time.mktime(mod_time)))
 
                 
 
@@ -304,7 +359,9 @@ def main():
     if args.a:
         with open(args.a, 'r') as f:
             for line in f:
-                if line != '\n':
+                if line[0] == '#' or line[0] == ';' or line[0] == ']':
+                    continue
+                elif line != '\n':
                     check_site(line)
     else:
         check_site(args.url)
