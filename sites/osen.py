bddb9e18a014b740dc7c9c7993bd4132693d3c5b
danrynr <daniel.reynard.k@gmail.com>
2023-03-04 11:55:48 +0700

    adding sites osen

---
+++ sites/osen.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import down.directory as dir
+
+def from_osen(hd):
+    r = requests.get(hd)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    wrap = soup.find('div', class_='detailTitle')
+    post_title = wrap.find('h1').text
+    post_date = soup.find('div', class_='detailTitle__post-infos').string.replace('-', '')[:8]
+    img_list = []
+
+    for item in soup.findAll('img', class_='view_photo'):
+        img_list.append(item.get('src'))
+
+    print("Found %s image(s)" % len(img_list))
+
+    dir.dir_handler(img_list, post_title, post_date)
