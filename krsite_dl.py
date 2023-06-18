004b0999669eb10e1d3c9388d5c64917420ddf3b
biscuits <neptunemist@proton.me>
2023-06-19 00:25:55 +0700

    small changes on site_dict

--- krsite_dl.py
+++ krsite_dl.py
@@ -1,8 +1,9 @@
 import argparse
 import sys
 from extractor import direct, generic
-from extractor.kr import dispatch, imbcnews, newsjamm, osen, sbs, sbsnews, mbc, naverpost, navernews, tvreport, kodyssey, tvjtbc, dazedkorea, cosmopolitan, marieclairekorea, lofficielkorea
+from extractor.kr import dispatch, imbcnews, newsjamm, osen, sbs, sbsnews, mbc, naverpost, navernews, tvreport, kodyssey, tvjtbc, newsen, dazedkorea, cosmopolitan, marieclairekorea, lofficielkorea
 from extractor.jp import nataliemu
+from extractor.sg import lofficielsingapore
 
 
 parser = argparse.ArgumentParser()
@@ -20,41 +21,68 @@ sitename = ''
 def check_site(url):
     site_dict = {
         # KOREAN SITES
-        'dispatch.co.kr': ['Dispatch', dispatch.from_dispatch],
-        'enews.imbc.com': ['iMBC News', imbcnews.from_imbcnews],
-        'mbc.co.kr': ['MBC', mbc.from_mbc],
-        'newsjamm.co.kr': ['News Jamm', newsjamm.from_newsjamm],
-        'osen.mt.co.kr': ['OSEN', osen.from_osen],
-        'programs.sbs.co.kr': ['SBS Program', sbs.from_sbs],
-        'ent.sbs.co.kr': ['SBS News', sbsnews.from_sbsnews],
-        'post.naver.com': ['Naver Post', naverpost.from_naverpost],
-        'news.naver.com': ['Naver News', navernews.from_navernews],
-        'tvreport.co.kr': ['TV Report', tvreport.from_tvreport],
-        'k-odyssey.com': ['K-odyssey', kodyssey.from_kodyssey],
-        'tv.jtbc.co.kr': ['JTBC TV', tvjtbc.from_tvjtbc],
-        'dazedkorea.com': ['Dazed Korea', dazedkorea.from_dazedkorea],
-        'cosmopolitan.co.kr': ['Cosmopolitan', cosmopolitan.from_cosmopolitan],
-        'marieclairekorea.com': ['Marie Claire Korea', marieclairekorea.from_marieclairekorea],
-        'lofficielkorea.com': ["L'officiel Korea", lofficielkorea.from_lofficielkorea],
-        # JAPAN SITES
-        'natalie.mu': ['Natalie 音楽ナタリー', nataliemu.from_nataliemu],
-        # SINGAPORE SITES
+        'KR': [{
+            'dispatch.co.kr': ['Dispatch', dispatch.from_dispatch],
+            'enews.imbc.com': ['iMBC News', imbcnews.from_imbcnews],
+            'mbc.co.kr': ['MBC', 'kr', mbc.from_mbc],
+            'newsjamm.co.kr': ['News Jamm', newsjamm.from_newsjamm],
+            'osen.mt.co.kr': ['OSEN', osen.from_osen],
+            'programs.sbs.co.kr': ['SBS Program', sbs.from_sbs],
+            'ent.sbs.co.kr': ['SBS News', sbsnews.from_sbsnews],
+            'post.naver.com': ['Naver Post', naverpost.from_naverpost],
+            'news.naver.com': ['Naver News', navernews.from_navernews],
+            'tvreport.co.kr': ['TV Report', tvreport.from_tvreport],
+            'k-odyssey.com': ['K-odyssey', kodyssey.from_kodyssey],
+            'tv.jtbc.co.kr': ['JTBC TV', tvjtbc.from_tvjtbc],
+            'newsen.com': ['Newsen', newsen.from_newsen],
+            'dazedkorea.com': ['Dazed Korea', dazedkorea.from_dazedkorea],
+            'cosmopolitan.co.kr': ['Cosmopolitan', cosmopolitan.from_cosmopolitan],
+            'marieclairekorea.com': ['Marie Claire Korea', marieclairekorea.from_marieclairekorea],
+            'lofficielkorea.com': ["L'officiel Korea", lofficielkorea.from_lofficielkorea],
+        }],
+        # JAPANESE SITES
+        'JP': [{
+            'natalie.mu': ['Natalie 音楽ナタリー', 'jp', nataliemu.from_nataliemu],
+        }],
+        # SINGAPOREAN SITES
+        'SG': [{
+            'lofficielsingapore.com': ["L'officiel Singapore", lofficielsingapore.from_lofficielsingapore],
+        }],
         # FALLBACK
-        'generic': ['Generic', generic.from_generic]
+        'FALLBACK': [{
+            'generic': ['Generic', generic.from_generic],
+        }]
     }
+    for country in site_dict:
+        location = country.upper()
+        # print(location)
+        for sites in site_dict[country]:
+            for site, site_info in sites.items():
+                if site in url:
+                    print("\n\033[1;31mSite name '%s'\033[0;0m" % site)
+                    print("\033[1;30;43mUrl: %s\033[0;0m" % url)
+                    site_info[1](url, location, site_info[0])
+                    return
 
-    for site in site_dict:
-        if site in url:
-            sitename = site_dict[site][0]
-            print("\n\033[1;31mSite name '%s'\033[0;0m" % site_dict[site][0])
-            print("\033[1;30;43mUrl: %s\033[0;0m" % url)
-            site_dict[site][1](url)
-            return
-    else:
-        print("\n\033[1;31mSite name '%s'\033[0;0m" % site_dict['generic'][0])
-        print("\030[1;30;43mUrl: %s\033[0;0m" % url)
-        site_dict['generic'][1](url)
-        return
+
+    # for sites in site_dict:
+    #     for site in sites:
+    #         country_code = site
+    #         print(country_code)
+    #         if site in url:
+    #             sitename = site_dict[site][0]
+                
+
+    #             print("\n\033[1;31mSite name '%s'\033[0;0m" % site_dict[site][0])
+    #             print("\033[1;30;43mUrl: %s\033[0;0m" % url)
+    #             print(site_dict[site][1])
+    #             # site_dict[site][1](url, site_dict[site][2])
+    #             return
+    # else:
+    #     print("\n\033[1;31mSite name '%s'\033[0;0m" % site_dict['generic'][0])
+    #     print("\030[1;30;43mUrl: %s\033[0;0m" % url)
+    #     site_dict['generic'][1](url)
+    #     return
     
 
 def main():
