d16c34e0f5d7fcdec9a8584ec398f3a0e38042b7
biscuits <neptunemist@proton.me>
2023-06-23 12:50:37 +0700

    newsen support

---
+++ extractor/kr/newsen.py
@@ -0,0 +1,33 @@
+import datetime
+from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import down.directory as dir
+
+def from_newsen(hd, loc, folder_name):
+    opt = Options()
+    opt.add_argument('--headless')
+    w = wd.Chrome(options=opt)
+    w.get(hd)
+
+    post_title = w.find_element(By.CSS_SELECTOR, 'span.art_title').text.strip()
+    post_date = w.find_element(By.XPATH, '//meta[@property="article:published_time"]').get_attribute('content').strip()
+    post_date = datetime.datetime.strptime(post_date, '%Y-%m-%d %H:%M:%S')
+    post_date_short = post_date.strftime('%y%m%d')
+
+    content = w.find_element(By.CLASS_NAME, 'article')
+    img_elements = content.find_elements(By.TAG_NAME, 'img')
+
+    img_list = []
+
+    for img_element in img_elements:
+        if not 'button' in img_element.get_attribute('src'):
+            img_list.append(img_element.get_attribute('src').replace('https', 'http'))
+
+    w.quit()
+
+    print("Title: %s" % post_title)
+    print("Date: %s" % post_date)
+    print("Found %s image(s)" % len(img_list))
+
+    dir.dir_handler(img_list, post_title, post_date_short, post_date, loc, folder_name)
