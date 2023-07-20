d4283b7830138bfe2a25ae70a759565ca576e566
biscuits <neptunemist@proton.me>
2023-07-20 11:33:49 +0700

    optimize news1 scripts

--- down/directory.py
+++ down/directory.py
@@ -1,7 +1,7 @@
 import os
 import re
 import platform
-from down.download import download_handler, download_handler_naver, download_handler_alt
+from down.download import download_handler, download_handler_naver, download_handler_alt, download_handler_news1
 import krsite_dl as kr
 
 # windows reserved characters
@@ -43,7 +43,22 @@ def dir_handler_alt(img_list, title = None, post_date_short = None, post_date = 
             os.makedirs(dirs)
 
     download_handler(img_list, dirs, post_date, loc)
-    
+
+# directory handling for news1 (news1.kr). Create a single directory, instead of multiple directories for each article we use the name of the article as image name
+def dir_handler_news1(img_list, title = None, post_date_short = None, post_date = None, loc = None, folder_name = None):
+    if not kr.args.no_windows_filenames:
+        title = re.sub(reserved_pattern, '', title)
+
+    if title != None and post_date_short != None:
+        dirs = os.path.join(kr.args.destination, 'krsite-dl', folder_name)
+        if not os.path.exists(dirs):
+            os.makedirs(dirs)
+    else:
+        dirs = os.path.join(kr.args.destination, 'krsite-dl', folder_name)
+        if not os.path.exists(dirs):
+            os.makedirs(dirs)
+
+    download_handler_news1(img_list, dirs, post_date, post_date_short, title, loc)
 
 # directory handling for naver blogs
 def dir_handler_naver(img_list, title = None, post_date_short = None, series = None, post_date = None, post_writer = None, folder_name = None):
