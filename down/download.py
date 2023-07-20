a825931fd91c1ed28fa3c8c0ace7ad3d94c06442
biscuits <neptunemist@proton.me>
2023-07-20 11:33:49 +0700

    optimize news1 scripts

--- down/download.py
+++ down/download.py
@@ -152,6 +152,70 @@ def download_handler_naver(img_list, dirs, post_date, chunk_size = 128):
         os.utime(dirs, (timestamp, timestamp))
 
 
+def download_handler_news1(img_list, dirs, post_date, post_date_short, title, loc, chunk_size = 128):
+    print("Downloading image(s) to folder: ", dirs)
+    for img in img_list:
+        try:
+            response = requests.head(img, timeout=60)
+        except requests.exceptions.Timeout:
+            print("Request timeout. Skipping...")
+        
+        content_length = int(response.headers.get('Content-Length', 0))
+        
+        img_ext = img.split('.')[-1]
+
+        img_name = f'{post_date_short} {title}.{img_ext}'
+
+        print("[Source URL] %s" % img)
+        print("[Image] %s" % img_name)
+
+        print("[Metadata] extracting metadata...")
+
+        if os.path.exists(dirs + '/' + img_name):
+            print("[Status] This file already exists. Skipping...")
+            continue
+
+        try:
+            current_size = os.path.getsize(dirs + "/" + img_name + '.part')
+        except FileNotFoundError:
+            current_size = 0
+        
+        if current_size < content_length:
+            headers = {'Range': f'bytes={current_size}-{content_length-1}'}
+            response = requests.get(img, headers=headers, stream=True)
+
+            with open(dirs + '/' + img_name + '.part', 'ab') as f:
+                start = time.time()
+                for chunk in response.iter_content(chunk_size=chunk_size):
+                    current_size += len(chunk)
+                    f.write(chunk)
+                    f.flush()
+                    progress_handler(current_size, content_length, start)
+            
+            os.rename(dirs + '/' + img_name + '.part', dirs + '/' + img_name)
+            
+            print("\n[Status] Image %s downloaded" % img_name)
+
+            # Set file and folders modification time
+            if loc == "KR":
+                utc = pytz.timezone("Asia/Seoul")
+            elif loc == "JP":
+                utc = pytz.timezone("Asia/Tokyo")
+            elif loc == "SG":
+                utc = pytz.timezone("Asia/Singapore")
+            
+            dt = post_date
+            dt = utc.localize(dt)
+            timestamp = int(dt.timestamp())
+            
+            # print(timestamp)
+            # print(dt)
+            print(timestamp)
+
+            os.utime(dirs + '/' + img_name, (timestamp, timestamp))
+        os.utime(dirs, (timestamp, timestamp))
+
+
 def download_handler_alt(img, dirs, chunk_size = 128):
     print("Downloading image(s) to folder: ", dirs)
     try:
