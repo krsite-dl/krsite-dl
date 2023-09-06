9dddced5470ce08594ba9091599422166aeaef73
biscuits <neptunemist@proton.me>
2023-09-06 17:46:15 +0700

    fix function name

--- extractor/kr/news1.py
+++ extractor/kr/news1.py
@@ -26,4 +26,4 @@ def from_news1(hd, loc, folder_name):
     print("Date: %s" % post_date)
     print("Found %s image(s)" % len(img_list))
 
-    dir.dir_handler_news1(img_list, post_title, post_date_short, post_date, loc, folder_name)
+    dir.dir_handler_no_folder(img_list, post_title, post_date_short, post_date, loc, folder_name)
