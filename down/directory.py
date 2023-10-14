21f57f9730370e9153d1b13872208d4891601cd4
biscuits <neptunemist@proton.me>
2023-10-14 23:53:20 +0700

    remove comment

--- down/directory.py
+++ down/directory.py
@@ -28,7 +28,6 @@ class DirectoryHandler:
     
 
     # directory handling for press sites / blogs that have distinct topics
-    # def handle_directory(self, img_list, title=None, post_date=None, post_date_short=None, loc=None, directory_name=None):
     def handle_directory(self, img_list, title=None, post_date=None, post_date_short=None, loc=None, directory_name=None):
         title, directory_name = self.__sanitize_string(title, directory_name)
 
