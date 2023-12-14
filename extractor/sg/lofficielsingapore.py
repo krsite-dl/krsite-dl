144f7a0082f4be6f88a86694a39191a9b23d9fad
biscuits <neptunemist@proton.me>
2023-12-14 19:29:14 +0700

    [fix] better extractor

--- extractor/sg/lofficielsingapore.py
+++ extractor/sg/lofficielsingapore.py
@@ -1,55 +1,41 @@
 import datetime
 import json
-import re
 
+from common.common_modules import SiteRequests, SiteParser
 from common.data_structure import Site, ScrapperPayload
-from pytz import timezone
-from selenium import webdriver as wd
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-from urllib.parse import urlparse, parse_qs, unquote
 
 SITE_INFO = Site(hostname="lofficielsingapore.com", name="L'Officiel Singapore", location="SG")
 
 def get_data(hd):
     img_list = []
 
-    opt = Options()
-    opt.add_argument('--headless')
-    w = wd.Chrome(options=opt)
+    site_parser = SiteParser()
+    site_requests = SiteRequests()
+    soup = site_parser._parse(site_requests.session.get(hd).text)
 
-    w.get(hd)
+    next_data = soup.find('script', id='__NEXT_DATA__').contents[0]
 
-    wait = WebDriverWait(w, 10)
+    json_data = json.loads(next_data)
 
-    article_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'section.article-layout__main')))
-    image_elements = article_element.find_elements(By.TAG_NAME, 'img')
+    post_infos = json_data['props']['pageProps']['subscription']['initialData']['article']
 
-    for image_element in image_elements:
-        wait.until(EC.visibility_of(image_element))
-        w.execute_script("arguments[0].scrollIntoView();", image_element)
-
-        srcset_value = image_element.get_attribute('src')
-        try:
-            url = parse_qs(urlparse(srcset_value).query)['url'][0].split('?')[0]
-            img_list.append(url)
-        except KeyError:
-            continue
-
-    script_tag = w.find_element(By.XPATH, '//script[@type="application/ld+json"]')
-    json_data = json.loads(script_tag.get_attribute('textContent'))
-
-    post_title = json_data['headline']
-    post_title = re.sub(r'&amp;|quot;', '', post_title)
-    post_date = json_data['datePublished']
+    post_title = post_infos['title']
+    post_date = post_infos['_publishedAt']
     post_date_short = post_date.replace('-', '')[2:8]
-    post_date = datetime.datetime.strptime(post_date, '%Y-%m-%dT%H:%M:%S.%fZ')
-    tz = timezone('Asia/Singapore')
-    post_date = post_date.astimezone(tz).replace(tzinfo=None)
+    # 2022-09-21T16:13:39+09:00
+    post_date = datetime.datetime.strptime(post_date, '%Y-%m-%dT%H:%M:%S%z')
+    post_date = post_date.replace(tzinfo=None)
 
-    w.quit()
+    # get post cover
+    img_list.append(post_infos['postCover'][0]['coverImage']['url'].split('?')[0])
+    post_blocks = post_infos['postBlocks']
+    for post_block in post_blocks:
+        if post_block['__typename'] == 'ImageBoxRecord':
+            img_list.append(post_block['image']['url'].split('?')[0])
+
+        if post_block['__typename'] == 'AdaptiveGalleryRecord':
+            for image in post_block['images']:
+                img_list.append(image['url'].split('?')[0])
 
     print("Title: %s" % post_title)
     print("Date: %s" % post_date)
