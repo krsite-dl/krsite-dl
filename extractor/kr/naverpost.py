05a48d24f5dc1b3bb37214e18a5ea129c5a04324
biscuits <neptunemist@proton.me>
2024-01-17 19:27:12 +0700

    [cleanup] remove selenium. [fix] extractor

--- extractor/kr/naverpost.py
+++ extractor/kr/naverpost.py
@@ -1,15 +1,13 @@
 """Extractor for https://post.naver.com"""
 
 import datetime
-import time
 import re
 import json
 import html
 
 from rich import print
-from selenium.common.exceptions import NoSuchElementException
 
-from common.common_modules import SeleniumParser, SiteRequests
+from common.common_modules import Requests
 from common.data_structure import Site, DataPayload
 from down.directory import DirectoryHandler
 
@@ -27,104 +25,132 @@ def get_data(hd):
 
     def naverpost_search(hd):
         """Get data from search result"""
-        parser = SeleniumParser()
-        w = parser._requests(hd)
-
-        post_list = set()
-
-        btn = w.find_element(parser.get_by('CLASS_NAME'), '_more')
-        stat = True
-        while stat == True:
-            try:
-                btn.click()
-                time.sleep(2)
-            except:
-                stat = False
-
-                for i in w.find_elements(parser.get_by('CLASS_NAME'), 'link_end'):
-                    post_list.add(i.get_attribute('href'))
-
-        w.quit()
-        print(f"Found {len(post_list)} post(s)")
-
-        for i in post_list:
-            naverpost_post(i)
+        print("Not supported yet")
 
     def naverpost_series(hd):
         """Get data from series page"""
-        parser = SeleniumParser()
-        w = parser._requests(hd)
+        member_no = hd.split('memberNo=')[1].split('&')[0]
 
-        series_list = set()
+        series_list_api = 'https://post.naver.com/async/series.naver'
+        params = {
+            'memberNo': member_no,
+            'postListViewType': 0,
+            'isExpertMy': 'true',
+        }
 
-        sender_name = w.find_element(parser.get_by('CLASS_NAME'), 'name').text
-        print("Sender: %s" % sender_name)
+        site_requests = Requests()
 
-        btn = w.find_element(parser.get_by('CLASS_NAME'), '_more')
-        stat = True
-        while stat == True:
-            try:
-                btn.click()
-                time.sleep(2)
-            except:
-                stat = False
+        series = []
 
-                for i in w.find_elements(parser.get_by('CLASS_NAME'), 'link'):
-                    series_list.add(i.get_attribute('href'))
+        while True:
+            req = site_requests.session.get(
+                series_list_api, params=params).text
 
-        w.quit()
-        print(f"Found {len(series_list)} series")
-        print("------------------\n")
+            nfn_val = req.split('"nextFromNo":')[1].split(',')[0].strip('"')
+            html_cont = req.split('"html":"')[1].split('","')[0]
+            html_cont = html.unescape(html_cont)
+            html_cont = html_cont.replace('\\n', '').replace(
+                '\\t', '').replace('\\', '')
 
-        for i in series_list:
+            for i in html_cont.split('<a href="')[1:]:
+                series.append("https://post.naver.com" + i.split('"')[0])
+
+            params['fromNo'] = nfn_val
+
+            if nfn_val == "":
+                break
+
+        site_requests.session.close()
+        print(f"Found {len(series)} series(s)")
+
+        for i in series:
             naverpost_list(i)
 
     def naverpost_list(hd):
         """Get data from series list"""
-        parser = SeleniumParser()
-        w = parser._requests(hd)
 
-        post_list = set()
+        member_no = hd.split('memberNo=')[1].split('&')[0]
+        series_no = hd.split('seriesNo=')[1].split('&')[0]
 
-        btn = w.find_element(parser.get_by('CLASS_NAME'), '_more')
-        stat = True
-        while stat == True:
-            try:
-                btn.click()
-                time.sleep(2)
-            except:
-                stat = False
-                for i in w.find_elements(parser.get_by('CLASS_NAME'), 'spot_post_area'):
-                    post_list.add(i.get_attribute('href'))
+        post_list_api = 'https://post.naver.com/my/series/detail/more.nhn'
+        params = {
+            'memberNo': member_no,
+            'seriesNo': series_no,
+            'lastSortOrder': 1,
+            'prevVolumeNo': '',
+            'fromNo': 1,
+            'totalCount': 0,
+        }
 
-        w.quit()
-        print(f"Found {post_list} post(s)")
+        site_requests = Requests()
 
-        for i in post_list:
+        post = []
+
+        while True:
+            req = site_requests.session.get(post_list_api, params=params).text
+            nfn_val = req.split('"nextFromNo":')[1].split(',')[0].strip('"')
+            html_cont = req.split('"html":"')[1].split('","')[0]
+            html_cont = html.unescape(html_cont)
+            html_cont = html_cont.replace('\\n', '').replace(
+                '\\t', '').replace('\\', '')
+
+            for i in html_cont.split('<a href="')[1:]:
+                post.append("https://post.naver.com" + i.split('"')[0])
+
+            params['fromNo'] = nfn_val
+
+            if nfn_val == "":
+                break
+
+        site_requests.session.close()
+        print(f"Found {len(post)} post(s)")
+
+        for i in post:
             naverpost_post(i)
 
     def naverpost_post(hd):
         """Get post data"""
-        site_requests = SiteRequests()
+        site_requests = Requests()
         site = site_requests.session.get(hd).text
 
-        post_writer = html.unescape(site.split("meta property=\"og:author\" content=\"")[1].split("\"")[0])
-        post_series = html.unescape(re.sub(r'<i[^>]*>.*?</i>', '', site.split('<div class="se_series">')[1].split('</div>')[0]).strip())
-        post_title = html.unescape(site.split("meta property=\"og:title\" content=\"")[1].split("\"")[0])
-        post_date = site.split("meta property=\"og:createdate\" content=\"")[1].split("\"")[0]
+        post_writer = html.unescape(site.split(
+            "meta property=\"og:author\" content=\"")[1]
+            .split("\"")[0]).strip()
+        try:
+            post_series = html.unescape(re.sub(
+                r'<i[^>]*>.*?</i>', '', site.split(
+                    '<div class="se_series">')[1]
+                .split('</div>')[0]).strip())
+        except IndexError:
+            post_series = html.unescape(site.split(
+                'class="series ">')[1]
+                .split('</a>')[0].strip())
+        post_series = re.sub(r'\s+', ' ', post_series)
+        post_title = html.unescape(site.split(
+            "meta property=\"og:title\" content=\"")[1].split("\"")[0].strip())
+        post_title = re.sub(r'\s+', ' ', post_title)
+        post_date = site.split("meta property=\"og:createdate\" content=\"")[
+            1].split("\"")[0].strip()
         post_date = datetime.datetime.strptime(post_date, '%Y.%m.%d. %H:%M:%S')
         post_date_short = post_date.strftime('%y%m%d')
 
         img_list = []
         for item in site.split("data-linkdata=\'")[1:]:
             linkdata = json.loads(item.split("\'>\n")[0])
-            if 'storep' not in linkdata['src']:
-                src = linkdata['src'].split('?')[0]
+            if 'src' in linkdata:
+                if 'storep' not in linkdata['src']:
+                    src = linkdata['src'].split('?')[0]
+                    img_list.append(src)
+
+        for item in site.split("data-realImagePath=")[1:]:
+            if 'storep' not in item:
+                src = item.split('?')[0]
                 img_list.append(src)
 
+        site_requests.session.close()
 
-        print(f"\nWriter: {post_writer}")
-        print(f"\nSeries: {post_series}")
+        print(f"Writer: {post_writer}")
+        print(f"Series: {post_series}")
         print(f"Title: {post_title}")
         print(f"Date: {post_date}")
         print(f"Found {len(img_list)} image(s)")
