2b0ebdd8cae454d82afa53c06cfc5a6111ec3232
Dan <neptunemist@proton.me>
2025-04-16 01:50:36 +0700

    fix: matching pattern for different url scenario

--- extractor/kr/sbs.py
+++ extractor/kr/sbs.py
@@ -14,6 +14,12 @@ SITE_INFO = Site(hostname="programs.sbs.co.kr", name="SBS Program")
 
 def get_data(hd):
     """Get data"""
+    p_visboards = re.compile(r'^https:\/\/programs\.sbs\.co\.kr\/[^\/]+\/[^\/]+\/(?:visualboards|multiboards)\/\d+(?:\/?)$')
+    p_visboards_s = re.compile(r'^https:\/\/programs\.sbs\.co\.kr\/[^\/]+\/[^\/]+\/(?:visualboards|multiboards)\/\d+\?[^#]*search_keyword=.*$')
+    p_visualboard = re.compile(r'^https:\/\/programs\.sbs\.co\.kr\/[^\/]+\/[^\/]+\/(?:visualboard|board)\/\d+\?(?=.*\bcmd=[^&#]+)(?=.*\bboard_no=\d+\b).*')
+    p_photos = re.compile(r'^https:\/\/programs\.sbs\.co\.kr\/[^\/]+\/[^\/]+\/photos\/\d+(?:\/?)$')
+    p_photos_s = re.compile(r'^https:\/\/programs\.sbs\.co\.kr\/[^\/]+\/[^\/]+\/photos\/\d+\?[^#]*search=.*$')
+    p_photo = re.compile(r'^https:\/\/programs\.sbs\.co\.kr\/[^\/]+\/[^\/]+\/photo\/\d+\/[a-f0-9]+\?(?=.*\balbumid=[a-f0-9]+\b)(?=.*\bphotoid=\d+\b).*')
 
     def get_time():
         ########### TOKEN############
@@ -94,6 +100,21 @@ def get_data(hd):
                 if key == vis_board_no:
                     code_temp.append(value)
         return code_temp
+    
+    def get_individual_info(hd):
+        """Get board post"""
+        site_req = Requests()
+        board_no = hd.split('board_no=')[-1].split('&')[0]
+        parent_name = re.search(
+            r'(?:(https|http)://)?(programs\.sbs\.co\.kr)(?:/[^/]+){1}/([^/?]+)', hd).group(3)
+        
+        # Get all board information
+        menu_api = "https://static.apis.sbs.co.kr/program-api/1.0/menu/"
+        menu_r = site_req.session.get(menu_api + parent_name).json()
+        site_req.session.close()
+        category = menu_r['program']['title']
+
+        return category, menu_r, board_no, parent_name
 
     def extract_info(url):
         path_name = re.search(
@@ -154,7 +175,7 @@ def get_data(hd):
         
         print(f"Found {len(boards)} post(s)")
         for i in boards:
-            get_board_post(i)
+            get_photo_album(i)
 
     def get_board_list(hd):
         """Get board list"""
@@ -226,137 +247,130 @@ def get_data(hd):
 
         print(f"Found {len(boards)} post(s)")
         for i in boards:
-            get_board_post(i)
+            get_visual_board(i)
 
-    def get_board_post(hd):
-        """Get board post"""
+    def get_photo_album(hd):
+        """Get photo album"""
+        category, menu_r, board_no, parent_name = get_individual_info(hd)
+        site_req = Requests()
+        bundle_id = re.search(r'(?:(https|http)://)?(programs\.sbs\.co\.kr)(?:/[^/]+){4}/([^/?]+)', hd).group(3)
+        photo_album_id = hd.split('albumid=')[-1].split('&')[0]
+        programid = menu_r['program']['programid']
+
+
+        photo_api = "https://static.apis.sbs.co.kr/photo-api/template/1.0/bundle/photo/{}/{}/{}/pc".format(programid, bundle_id, photo_album_id)
+
+        r = site_req.session.get(photo_api)
+        json_data = r.json()
+
+        data = json_data['photo']
+        post_title = data['title']
+        post_date = data['reg_date'].strip()
+        post_date = datetime.datetime.strptime(post_date, '%Y.%m.%d')
+        post_date_short = post_date.strftime('%y%m%d')
+        
+        img_list = set()
+
+        img_list.add(re.sub(r'https?://|://|//', 'https://', data['image_url']))
+
+        for i in json_data['list']:
+            img_list.add(i['full_size_image_url'])
+        
+        site_req.session.close()
+        print(f"Title: {post_title}")
+        print(f"Date: {post_date}")
+        print(f"Found {len(img_list)} image(s)")
+
+        dir = [SITE_INFO.name, category, f"{post_date_short} {post_title}"]
+
+        payload = DataPayload(
+            directory_format=dir,
+            media=img_list,
+            option=None,
+            custom_headers=None
+        )
+
+        DirectoryHandler().handle_directory(payload)
+
+    def get_visual_board(hd):
+        """Get visual board"""
+        category, menu_r, board_no, parent_name = get_individual_info(hd)
+        site_req = Requests()
+        vis_board_no = re.search(
+            r'(?:(https|http)://)?(programs\.sbs\.co\.kr)(?:/[^/]+){3}/([^/?]+)', hd).group(3)
+    
+        code_temp = get_board_menu(vis_board_no, parent_name)
+
+        print(f"Board no: {board_no}")
 
         site_req = Requests()
-        board_no = hd.split('board_no=')[-1].split('&')[0]
-        parent_name = re.search(
-            r'(?:(https|http)://)?(programs\.sbs\.co\.kr)(?:/[^/]+){1}/([^/?]+)', hd).group(3)
-        
-        # Get all board information
-        menu_api = "https://static.apis.sbs.co.kr/program-api/1.0/menu/"
-        menu_r = site_req.session.get(menu_api + parent_name).json()
+    
+        for i in code_temp:
+            code = i
+            api = f"https://api.board.sbs.co.kr/bbs/V2.0/basic/board/detail/{board_no}"
+
+            params = {
+                'callback': f'boardViewCallback_{code}',
+                'action_type': 'callback',
+                'board_code': code,
+                'jwt-token': '',
+                '_': get_time()
+            }
+
+            r = site_req.session.get(api, params=params)
+
+            if 'err_code' not in r.text:
+                break
+
+        json_data = r.text
+        json_data = json_data.split('boardViewCallback_%s(' % code)[1]
+        json_data = json_data.rstrip(');')
+        json_data = json.loads(json_data)
+
+        data = json_data['Response_Data_For_Detail']
+        post_title = data['TITLE'].strip()
+        post_date = data['REG_DATE'].strip()
+        post_date = datetime.datetime.strptime(post_date, '%Y-%m-%d %H:%M:%S')
+        post_date_short = post_date.strftime('%y%m%d')
+
+        img_list = set()
+
+        for i in data['URL']:
+            if 'http' not in i:
+                img_list.add('http:' + str(i))
+            else:
+                img_list.add(str(i))
+
         site_req.session.close()
-        category = menu_r['program']['title']
+        print(f"Title: {post_title}")
+        print(f"Date: {post_date}")
+        print(f"Found {len(img_list)} image(s)")
 
-        def photo_album():
-            """Get photo album"""
-            bundle_id = re.search(r'(?:(https|http)://)?(programs\.sbs\.co\.kr)(?:/[^/]+){4}/([^/?]+)', hd).group(3)
-            photo_album_id = hd.split('albumid=')[-1].split('&')[0]
-            programid = menu_r['program']['programid']
+        dir = [SITE_INFO.name, category, f"{post_date_short} {post_title}"]
 
+        payload = DataPayload(
+            directory_format=dir, 
+            media=img_list,
+            option=None,
+            custom_headers={'Referer': 'https://programs.sbs.co.kr'}
+        )
 
-            photo_api = "https://static.apis.sbs.co.kr/photo-api/template/1.0/bundle/photo/{}/{}/{}/pc".format(programid, bundle_id, photo_album_id)
+        DirectoryHandler().handle_directory(payload)
 
-            r = site_req.session.get(photo_api)
-            json_data = r.json()
+    if p_visualboard.match(hd):
+        get_visual_board(hd)
 
-            data = json_data['photo']
-            post_title = data['title']
-            post_date = data['reg_date'].strip()
-            post_date = datetime.datetime.strptime(post_date, '%Y.%m.%d')
-            post_date_short = post_date.strftime('%y%m%d')
-            
-            img_list = set()
+    if p_photo.match(hd):
+        get_photo_album(hd)
 
-            img_list.add(re.sub(r'https?://|://|//', 'https://', data['image_url']))
-
-            for i in json_data['list']:
-                img_list.add(i['full_size_image_url'])
-            
-            site_req.session.close()
-            print(f"Title: {post_title}")
-            print(f"Date: {post_date}")
-            print(f"Found {len(img_list)} image(s)")
-
-            dir = [SITE_INFO.name, category, f"{post_date_short} {post_title}"]
-
-            payload = DataPayload(
-                directory_format=dir,
-                media=img_list,
-                option=None,
-                custom_headers=None
-            )
-
-            DirectoryHandler().handle_directory(payload)
-
-        def visual_board():
-            """Get visual board"""
-            vis_board_no = re.search(
-                r'(?:(https|http)://)?(programs\.sbs\.co\.kr)(?:/[^/]+){3}/([^/?]+)', hd).group(3)
-        
-            code_temp = get_board_menu(vis_board_no, parent_name)
-
-            print(f"Board no: {board_no}")
-
-            site_req = Requests()
-        
-            for i in code_temp:
-                code = i
-                api = f"https://api.board.sbs.co.kr/bbs/V2.0/basic/board/detail/{board_no}"
-
-                params = {
-                    'callback': f'boardViewCallback_{code}',
-                    'action_type': 'callback',
-                    'board_code': code,
-                    'jwt-token': '',
-                    '_': get_time()
-                }
-
-                r = site_req.session.get(api, params=params)
-
-                if 'err_code' not in r.text:
-                    break
-
-            json_data = r.text
-            json_data = json_data.split('boardViewCallback_%s(' % code)[1]
-            json_data = json_data.rstrip(');')
-            json_data = json.loads(json_data)
-
-            data = json_data['Response_Data_For_Detail']
-            post_title = data['TITLE'].strip()
-            post_date = data['REG_DATE'].strip()
-            post_date = datetime.datetime.strptime(post_date, '%Y-%m-%d %H:%M:%S')
-            post_date_short = post_date.strftime('%y%m%d')
-
-            img_list = set()
-
-            for i in data['URL']:
-                if 'http' not in i:
-                    img_list.add('http:' + str(i))
-                else:
-                    img_list.add(str(i))
-
-            site_req.session.close()
-            print(f"Title: {post_title}")
-            print(f"Date: {post_date}")
-            print(f"Found {len(img_list)} image(s)")
-
-            dir = [SITE_INFO.name, category, f"{post_date_short} {post_title}"]
-
-            payload = DataPayload(
-                directory_format=dir, 
-                media=img_list,
-                option=None,
-                custom_headers={'Referer': 'https://programs.sbs.co.kr'}
-            )
-
-            DirectoryHandler().handle_directory(payload)
-
-
-        if 'photo' in hd:
-            photo_album()
-        else:
-            visual_board()
-
-
-
-    if 'search_keyword=' in hd:
+    if p_visboards_s.match(hd):
         get_board_list(hd)
-    elif 'search=' in hd and 'photo' in hd:
+
+    if p_visboards.match(hd):
+        get_board_list(hd)
+
+    if p_photos_s.match(hd):
         get_photo_list(hd)
-    else:
-        get_board_post(hd)
+
+    if p_photos.match(hd):
+        get_photo_list(hd)
